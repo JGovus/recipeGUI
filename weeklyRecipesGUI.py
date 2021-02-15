26e8f8916ec5e53361da8cd889942a300a1fa459
JGovus <joseph.govus@bobcats.gcsu.edu>
2021-02-15 14:43:15 -0500

    Update for group members.

--- weeklyRecipesGUI.py
+++ weeklyRecipesGUI.py
@@ -2,8 +2,9 @@ from tkinter import *
 
 root = Tk()
 root.title("Weekly Recipes w/ Ingredients")
-root.geometry("800x900")
+root.geometry("1200x800")
 
 print("hello world lolol")
+print("Testing for group members.")
 
 root.mainloop()
