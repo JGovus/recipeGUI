e6e7e476b31d7f4a14a3874639b04d017217c04b
Owner <ryanmell95@gmail.com>
2021-02-17 14:26:05 -0500

    label

--- weeklyRecipesGUI.py
+++ weeklyRecipesGUI.py
@@ -4,7 +4,7 @@ root = Tk()
 root.title("Weekly Recipes w/ Ingredients")
 root.geometry("1200x800")
 
-print("hello world lolol")
-print("Testing for group members.")
+my_label1 = Label(root, text="Day 1", fg="white", bg="black", font=("Times New Roman", 28))
+my_label1.grid(row=0, column=0, columnspan=1)
 
 root.mainloop()
